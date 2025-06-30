ccd2d8100d8338a96d1723dc9669df9cadcf93cd
Ibra <Ibrapku12@gmail.com>
2025-06-30 07:52:56 +0700

    Add project files.

---
+++ ProjectFIRP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectFIRP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
